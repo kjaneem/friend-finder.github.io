c4022474af0c192d070f073c584f0aa472fafea1
Karen Manley <Karen Manley>
2017-07-22 20:20:34 -0400

    remove unnecessary files

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 var PORT = process.env.PORT || 1962;
+
+//Present friend-finder page
+//When "Go to Survey" button is pushed
+//  Present Survey page
+//      Accept user input
+//      When "Submit" button is pushed
+//          Edit for name and pic link
+//          if survey data OK
+//              Generate survey score
+//              For every stored survey score
+//                  Determine if match
+//              Write survey data
+//          else
+//              Present Error pop-up
