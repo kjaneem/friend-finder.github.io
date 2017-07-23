30e37cfbbd827241b3935d2f5f0e60134a7a13d9
Karen Manley <Karen Manley>
2017-07-23 14:46:57 -0400

    add pics

---
+++ app/data/surveyData.js
@@ -0,0 +1,24 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the surveys.
+// ===============================================================================
+
+var surveyArray = [
+  {
+    name: "",
+    photo: "",
+    q1: "",
+    q2: "",
+    q3: "",
+    q4: "",
+    q5: "",
+    q6: "",
+    q7: "",
+    q8: "",
+    q9: "",
+    q10: ""
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = surveyArray;
